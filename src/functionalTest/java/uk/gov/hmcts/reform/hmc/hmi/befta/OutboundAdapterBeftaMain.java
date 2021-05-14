acf57c8ef8e203e62ff7a84eaa69ebbca96bb64b
RBaker <rebecca.baker@hmcts.net>
2021-05-14 11:33:42 +0100

    RDM-12276 fix checkstyle

--- src/functionalTest/java/uk/gov/hmcts/reform/hmc/hmi/befta/OutboundAdapterBeftaMain.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/hmc/hmi/befta/OutboundAdapterBeftaMain.java
@@ -5,10 +5,12 @@ import uk.gov.hmcts.befta.BeftaMain;
 public class OutboundAdapterBeftaMain {
 
     private OutboundAdapterBeftaMain() {
-        // Hide Utility Class Constructor : Utility classes should not have a public or default constructor (squid:S1118)
+        // Hide Utility Class Constructor :
+        // Utility classes should not have a public or default constructor (squid:S1118)
     }
 
     public static void main(String[] args) {
+
         BeftaMain.main(args, new OutboundAdapterTestAutomationAdapter());
     }
 
