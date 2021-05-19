41068926805c9536c37bbda35510272b0cf69e21
RBaker <rebecca.baker@hmcts.net>
2021-05-19 10:57:08 +0100

    RDM-12276 updated befta implementation

--- src/functionalTest/java/uk/gov/hmcts/reform/hmc/hmi/befta/OutboundAdapterTestAutomationAdapter.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/hmc/hmi/befta/OutboundAdapterTestAutomationAdapter.java
@@ -1,31 +1,47 @@
 package uk.gov.hmcts.reform.hmc.hmi.befta;
 
 import uk.gov.hmcts.befta.BeftaTestDataLoader;
-import uk.gov.hmcts.befta.DefaultBeftaTestDataLoader;
-import uk.gov.hmcts.befta.DefaultTestAutomationAdapter;
-import uk.gov.hmcts.befta.dse.ccd.TestDataLoaderToDefinitionStore;
+import uk.gov.hmcts.befta.TestAutomationAdapter;
+import uk.gov.hmcts.befta.data.UserData;
 import uk.gov.hmcts.befta.player.BackEndFunctionalTestScenarioContext;
 
-public class OutboundAdapterTestAutomationAdapter extends DefaultTestAutomationAdapter {
+import java.util.concurrent.ExecutionException;
 
-    private TestDataLoaderToDefinitionStore loader = new TestDataLoaderToDefinitionStore(this);
+public class OutboundAdapterTestAutomationAdapter implements TestAutomationAdapter {
+
+
+    @Override
+    public String getNewS2SToken() {
+        return null;
+    }
+
+    @Override
+    public String getNewS2SToken(String clientId) {
+        return null;
+    }
+
+    @Override
+    public void authenticate(UserData user, String preferredTokenClientId) throws ExecutionException {
+
+    }
 
     @Override
     public Object calculateCustomValue(BackEndFunctionalTestScenarioContext scenarioContext, Object key) {
-        if (key.toString().startsWith("no_dynamic_injection_")) {
-            return key.toString().replace("no_dynamic_injection_","");
-        }
-        return super.calculateCustomValue(scenarioContext, key);
+        return null;
     }
 
     @Override
-    protected BeftaTestDataLoader buildTestDataLoader() {
-        return new DefaultBeftaTestDataLoader() {
+    public BeftaTestDataLoader getDataLoader() {
+        return new BeftaTestDataLoader() {
             @Override
-            public void doLoadTestData() {
-                // NB: no CCD test data load requirements
+            public void loadTestDataIfNecessary() {
+
+            }
+
+            @Override
+            public boolean isTestDataLoadedForCurrentRound() {
+                return false;
             }
         };
     }
-
 }
