4dac48bb8f761f8b498d737016b7437f958e7bad
RBaker <rebecca.baker@hmcts.net>
2021-06-29 15:22:50 +0100

    HMAN-7 refactoring

--- src/main/java/uk/gov/hmcts/reform/hmc/config/MessageProcessor.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/config/MessageProcessor.java
@@ -7,7 +7,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClientException;
 import uk.gov.hmcts.reform.hmc.repository.DefaultFutureHearingRepository;
