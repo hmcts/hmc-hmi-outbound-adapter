2c32c0452c8184d6a2e5efb720cc138454cc00f6
markdathorne <mark.dathorne@cgi.com>
2022-07-01 17:04:20 +0100

    HMAN-312 Suppress CVE-2022-33915,CVE-2022-34305 & trigger build

--- src/main/java/uk/gov/hmcts/reform/hmc/Application.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/Application.java
@@ -12,7 +12,7 @@ import java.time.Clock;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @EnableFeignClients
-@SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, it's not a utility class
+@SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, it is not a utility class
 public class Application {
     public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
