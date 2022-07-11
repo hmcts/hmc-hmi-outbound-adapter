c3defa0fb977084265171774fcd8cc3b33aa265c
marvincudjoe <18750678+marvincudjoe@users.noreply.github.com>
2022-07-11 12:58:14 +0100

    HMAN-347 - remove cves from suppressions.xml

--- src/main/java/uk/gov/hmcts/reform/hmc/config/OpenApiConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/config/OpenApiConfiguration.java
@@ -15,13 +15,15 @@ public class OpenApiConfiguration {
     @Bean
     public OpenAPI openApi() {
         return new OpenAPI()
-            .info(new Info().title("CFT Hearing Service")
-                .version("v0.0.1")
-                .license(new License().name("MIT").url("https://opensource.org/licenses/MIT")))
-            .info(new Info().contact(new Contact()
-                .name("CDM")
-                .url("https://tools.hmcts.net/confluence/display/RCCD/Reform%3A+Case+Management+Home")
-                .email("corecasedatateam@hmcts.net")))
+            .info(new Info()
+                .title("HMC HMI Outbound Adapter")
+                .version("v1.0.0")
+                .contact(new Contact()
+                    .name("CDM")
+                    .url("https://tools.hmcts.net/confluence/display/RCCD/Reform%3A+Case+Management+Home")
+                    .email("corecasedatateam@hmcts.net"))
+                .license(new License().name("MIT").url("https://opensource.org/licenses/MIT"))
+            )
             .externalDocs(new ExternalDocumentation()
                 .description("README")
                 .url("https://github.com/hmcts/hmc-cft-hearing-service#readme"));
