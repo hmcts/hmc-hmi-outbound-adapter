9a7eee205666e9b9481dcfd6ac5b3054267387dd
Antony Leons <antony@leons.dev>
2024-04-04 09:34:54 +0100

    fix build

--- src/main/java/uk/gov/hmcts/reform/hmc/ApplicationParams.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/ApplicationParams.java
@@ -2,9 +2,12 @@ package uk.gov.hmcts.reform.hmc;
 
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
+import uk.gov.hmcts.reform.hmc.exceptions.ServiceException;
 
 import javax.inject.Named;
 import javax.inject.Singleton;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 @Getter
 @Named
@@ -41,9 +44,28 @@ public class ApplicationParams {
     @Value("${azure.jms.servicebus.inbound-connection-string}")
     private String inboundConnectionString;
 
+    @Value("${role.assignment.api.host}")
+    private String roleAssignmentServiceHost;
+
     @Value("${azure.jms.servicebus.exponential-multiplier}")
     private String exponentialMultiplier;
 
     @Value("${azure.jms.servicebus.max-retry-attempts}")
     private int maxRetryAttempts;
+
+    public static String encode(final String stringToEncode) {
+        try {
+            return URLEncoder.encode(stringToEncode, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new ServiceException(e.getMessage());
+        }
+    }
+
+    public String roleAssignmentBaseUrl() {
+        return roleAssignmentServiceHost + "/am/role-assignments";
+    }
+
+    public String amGetRoleAssignmentsUrl() {
+        return roleAssignmentBaseUrl() + "/actors/{uid}";
+    }
 }
