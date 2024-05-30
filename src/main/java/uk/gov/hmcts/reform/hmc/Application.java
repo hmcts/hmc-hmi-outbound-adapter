87f752df4554de71410925331ba2da4aaede924e
Antony Leons <antony@leons.dev>
2024-05-30 09:12:43 +0100

    Update Application.java

--- src/main/java/uk/gov/hmcts/reform/hmc/Application.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/Application.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.time.Clock;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableScheduling
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, it is not a utility class
 public class Application {
     public static void main(final String[] args) {
