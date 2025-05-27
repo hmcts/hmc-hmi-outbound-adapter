a0002fd70d69af62adf7545ff5061dd93469fd30
padmapriyanalam <padmapriya.nalam@gmail.com>
2025-05-27 12:24:04 +0100

    HMAN-997 - potential duplicate request being sent to HMI

--- src/main/java/uk/gov/hmcts/reform/hmc/repository/PendingRequestRepository.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/repository/PendingRequestRepository.java
@@ -7,13 +7,11 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 import uk.gov.hmcts.reform.hmc.data.PendingRequestEntity;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Transactional
 @Repository("pendingRequestRepository")
 public interface PendingRequestRepository extends CrudRepository<PendingRequestEntity, Long> {
 
