7516fdead9675f2ddcf34e7f4b0bf9f824521539
padmapriyanalam <padmapriya.nalam@gmail.com>
2025-06-10 15:54:14 +0100

    HMAN-1000 - Fix SonarQube code coverage issues

--- src/main/java/uk/gov/hmcts/reform/hmc/data/HearingDayDetailsEntity.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/data/HearingDayDetailsEntity.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.hmc.data;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -14,7 +13,6 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.ToString;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
@@ -48,13 +46,9 @@ public class HearingDayDetailsEntity extends BaseEntity implements Serializable 
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "hearing_response_id")
-    @ToString.Exclude
-    @JsonBackReference
     private HearingResponseEntity hearingResponse;
 
     @OneToMany(mappedBy = "hearingDayDetails", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    @ToString.Exclude
-    @JsonBackReference
     private List<HearingDayPanelEntity> hearingDayPanel;
 
 }
