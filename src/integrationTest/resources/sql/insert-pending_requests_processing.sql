05fcfd4cd805e6abb80706dbd15f871bd2f93aa4
padmapriyanalam <padmapriya.nalam@gmail.com>
2025-07-16 16:28:16 +0100

    HMAN-1001 - Add SEQUENCE id to all the insert statements

--- src/integrationTest/resources/sql/insert-pending_requests_processing.sql
+++ src/integrationTest/resources/sql/insert-pending_requests_processing.sql
@@ -1,3 +1,3 @@
-INSERT INTO public.pending_requests (
+INSERT INTO public.pending_requests (id,
     hearing_id, message, message_type, status, incident_flag, version_number, last_tried_date_time, submitted_date_time, retry_count, deployment_id)
-VALUES (2000000001, 'Should be this one', 'REQUEST_HEARING', 'PROCESSING', false, 1, '2023-10-01 10:00:00', '2023-10-01 10:00:00', 1, 'depId01');
+VALUES (1, 2000000001, 'Should be this one', 'REQUEST_HEARING', 'PROCESSING', false, 1, '2023-10-01 10:00:00', '2023-10-01 10:00:00', 1, 'depId01');
