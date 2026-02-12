3037415f95fd3c3929a881a70a70c1f94768dafb
Ben Lang <132359359+lang-ben@users.noreply.github.com>
2026-02-12 13:03:44 +0000

    CCD-6618 : Reduce unnecessary dependencies (#265)
    * CCD-6618 : Reduce unnecessary dependencies
    
    * Fix integration tests
    
    * Fix checkstyle
    
    * disable ssl healthcheck data
    
    * Remove commented out dependencies & change to latest junit
    
    * add back in logging-appinsights
    
    ---------
    
    Co-authored-by: dinesh1patel <74076102+dinesh1patel@users.noreply.github.com>

--- src/integrationTest/java/uk/gov/hmcts/reform/hmc/BaseTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/hmc/BaseTest.java
@@ -4,10 +4,10 @@ import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.cloud.contract.wiremock.WireMockConfigurationCustomizer;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import uk.gov.hmcts.reform.hmc.config.MessageSenderToTopicConfiguration;
 
 @SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -16,7 +16,7 @@ import uk.gov.hmcts.reform.hmc.config.MessageSenderToTopicConfiguration;
 @ActiveProfiles("itest")
 public class BaseTest implements WireMockConfigurationCustomizer {
 
-    @MockBean
+    @MockitoBean
     protected MessageSenderToTopicConfiguration messageSenderToTopicConfiguration;
 
     @Value("${wiremock.server.port}")
