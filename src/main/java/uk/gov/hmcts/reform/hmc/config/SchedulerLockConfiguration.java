dae279df0cd6c74cb0498446d479bdaf41c80183
marco <marco.collura@hmcts.net>
2026-09-23 11:26:29 +0100

    Fix imports

--- src/main/java/uk/gov/hmcts/reform/hmc/config/SchedulerLockConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/config/SchedulerLockConfiguration.java
@@ -1,12 +1,12 @@
 package uk.gov.hmcts.reform.hmc.config;
 
-import javax.sql.DataSource;
 import net.javacrumbs.shedlock.core.LockProvider;
 import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SchedulerLockConfiguration {
